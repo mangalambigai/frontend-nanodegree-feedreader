e1a0bd9b688efbeab70e3ca35fc4f20c7b447ff3
mangalambigai <mangalambigais@gmail.com>
2015-12-04 14:59:59 -0500

    Add test for future feature

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 
 - Launch index.html
 - The rss feeds are displayed, and the jasmine test results are shown at the bottom of the page.
+
+# Test specs for future enhancements:
+- Click Counter test suite expects an element of class 'count' to be present inside the 'entry' class.
+- The second test checks if the content of this element is numeric.
+- Adding an element with class count and a numeric value should pass these tests.
+
+```
+                <article class="entry">
+                    <h2>{{title}}</h2>
+                    <p>{{contentSnippet}}</p>
+                    <span class="count">3</span>
+                </article>
+```
+
+- A non numeric value should fail the test.
+
+```
+                <article class="entry">
+                    <h2>{{title}}</h2>
+                    <p>{{contentSnippet}}</p>
+                    <span class="count">abc</span>
+                </article>
+```
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -92,9 +92,9 @@ $(function() {
          * a single .entry element within the .feed container.
          */
         it('are loaded', function(done) {
-            var $firstentry = $('.feed .entry');
-            expect($firstentry.length).not.toBe(0);
-            val = $firstentry.html();
+            var $entries = $('.feed .entry');
+            expect($entries).not.toBe(0);
+            val = $entries.html();
             done();
         });
     });
@@ -111,11 +111,37 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          */
         it('changes the content', function(done) {
-            var $firstentry = $('.feed .entry');
-            expect($firstentry.length).not.toBe(0);
-            var val2 = $firstentry.html();
+            var $entries = $('.feed .entry');
+            expect($entries.length).not.toBe(0);
+            var val2 = $entries.html();
             expect(val2).not.toBe(val);
             done();
         });
+    });
+
+    /* This test suite tests a future enhancement -
+     * a counter that shows how many times the entry's link is clicked.
+     */
+    describe('Click Counter', function() {
+
+
+        /* This test ensures that the counter is present and not empty*/
+        it('is present', function() {
+            var $countelements = $('.feed .entry .count');
+            expect($countelements).toBeDefined();
+            expect($countelements.length).not.toBe(0);
+            $countelements.each(function() {
+                expect($(this).text()).not.toBe('');
+            });
+        });
+
+        /* This test ensures that the counter has a number*/
+        it('shows a number', function() {
+            var $countelements = $('.feed .entry .count');
+            $countelements.each(function() {
+                expect(Number($(this).text())).not.toBeNaN();
+            });
+        });
+
     });
 }());
